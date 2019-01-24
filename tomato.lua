218e3647721d5716bb4a3946a8b6d38ebecf1e04
samcompu <3340751717@qq.com>
2019-01-24 14:36:50 +0800

    Build a beatiful progress

--- tomato.lua
+++ tomato.lua
@@ -15,42 +15,54 @@ local put_off_work_time = 0;
 local totale_work_time = 0;
 local totale_rest_time = 0;
 
--- Run command in shell and return the signal code
-run = function(command)
-	val, infor, code = os.execute(command);
+local time_block = 0;
+local begin_time = {};
+local plan_to_do = {};
+local have_done = {};
+local end_time = {};
+
+-- code_back_run() | Run command in shell and return the signal code {{{
+code_back_run = function(command)
+	local val, infor, code = os.execute(command);
 	return code;
 end
+-- }}}
 
--- Turn float to int
+-- out_back_run() | Run command in shell and return its output {{{
+out_back_run = function(command)
+	local file = io.popen(command, "r");
+	local content = file:read("*a");
+	io.close(file);
+	return content;
+end
+-- }}}
+
+-- to_int() | Turn float to int {{{
 to_int = function(num)
 	if string.find(num, "%.") == nil then return num; end
 	return string.sub(num, 1, string.find(num, "%.") - 1); 
 end
+-- }}}
 
--- Check if the path is exist
-check_path = function(path) return run("test -e " .. path) == 0; end
+-- check_path() | Check if the path is exist
+check_path = function(path) return code_back_run("test -e " .. path) == 0; end
 
 get_second = function() return os.date("%H") * 3600 + os.date("%M") * 60 + os.date("%S"); end
 
 get_time = function() return os.date("%H") .. ":" .. os.date("%M"); end
 
--- Get and format the things which wrote to /tmp/.thing
+-- format_thing() | Format the things which plan to do and have done {{{
 format_thing = function()
-	local file = io.open(thing_file, "r");
 	local result = "";
-	while true do
-		local begin_time = file:read("*l");
-		if begin_time == nil then break; end
-		local plan_to_do = file:read("*l");
-		if plan_to_do == "" then plan_to_do = "Unknow"; end
-		local have_done = file:read("*l");
-		if have_done == "" then have_done = "Unknow"; end
-		local end_time = file:read("*l");
-		result = result .. "[" .. begin_time .. " - " .. end_time .. "] " .. have_done .. "(" .. plan_to_do .. ")\n";
+	for i = 1, time_block - 1, 1 do
+		result = result .. "[" .. begin_time[i] .. " - " .. end_time[i] .. "] " .. have_done[i];
+		if have_done[i] == plan_to_do[i] then
+			result = result .. "(" .. plan_to_do[i] .. ")\n";
+		end
 	end
-	run("rm -r " .. thing_file);
 	return result;
 end
+-- }}}
 
 -- Write summary file to /summary/xxx_xxx.txt
 write_file = function()
@@ -76,7 +88,7 @@ end
 zenity = function(kind, ok, cancel, text, ...)
 	local command = "zenity --" .. kind .. " --title='" .. title .. "' --cancel-label='" .. ok .. "' --ok-label='" .. cancel .. "' --text='" .. text .. "' --default-cancel";
 	for i, v in ipairs{...} do command = command .. " " .. v; end
-	return run(command);
+	return code_back_run(command);
 end
 
 -- Node the data
@@ -90,7 +102,7 @@ end
 
 -- Exit the application
 exit = function()
-	local choose = zenity("question", "Yes", "No", "Are you sure to exit this application?", "--no-wrap");
+	local choose = code_back_run("zenity --question --title='" .. title .. "' --cancel-label='Yes' --ok-label='No' --text='Are you sure to exit this application?' --default-cancel --no-wrap");
 	if choose == 1 then 
 		note();
 		write_file();
@@ -113,70 +125,87 @@ end
 
 -- Set up the basic folder
 init = function()
-	if check_path(config_path) == false then run("mkdir -p " .. config_path); end
-	if check_path(summary_path) == false then run("mkdir -p " .. summary_path);end
-	if check_path(tmp_path) == false then run("mkdir -p " .. tmp_path);end
+	if check_path(config_path) == false then code_back_run("mkdir -p " .. config_path); end
+	if check_path(summary_path) == false then code_back_run("mkdir -p " .. summary_path);end
+	if check_path(tmp_path) == false then code_back_run("mkdir -p " .. tmp_path);end
 end
 
 -- Count the rest of time
 count_time = function(need_time)
-	local cnt = 1;
-	while cnt <= 100 do
-		os.execute("sleep " .. need_time / 100);
-		local left = math.ceil(need_time - cnt * (need_time / 100));
-		print("# Back in " .. to_int(left / 60) .. " m " .. to_int(left % 60) .. " s");
-		print(cnt);
-		cnt = cnt + 1;
+	need_time = tonumber(need_time);
+	local cnt = 0;
+	local sec = 0;
+	if need_time > 60 then
+		while sec <= need_time do
+			local left = need_time - sec;
+			print("# Back in " .. to_int(left / 60) .. " m " .. to_int(left % 60) .. " s");
+			print(cnt);
+			os.execute("sleep 1");
+			sec = sec + 1;
+			cnt = math.floor(sec * 100 / need_time);
+		end
+	else
+		while cnt <= 100 do
+			local left = need_time - sec;
+			print("# Back in " .. to_int(left / 60) .. " m " .. to_int(left % 60) .. " s");
+			print(cnt);
+			os.execute("sleep " .. need_time / 100);
+			cnt = cnt + 1;
+			sec = math.floor(cnt * (need_time / 100));
+		end
 	end
 end
 
-main = function()
-	init();
+loop = function()
 	local choose;
+	time_block = time_block + 1;
+
+	-- Plan to do
 	put_off_work_time = put_off_work_time + timing(function()
-		choose = ready("work");
-		while choose == 0 do 
-			exit();
-			choose = ready("work");
+	  plan_to_do[time_block] = out_back_run("zenity --entry --text='Make a plan and start work' --ok-label='Start' --cancel-label='Exit'");
+		while plan_to_do[time_block] == "\n" or plan_to_do[time_block] == "" do
+			if plan_to_do[time_block] == "\n" then
+				plan_to_do[time_block] = out_back_run("zenity --entry --text='Make a plan first please :)' --ok-label='Start' --cancel='Exit'");
+			end
+			if plan_to_do[time_block] == "" then 
+				exit(); -- The only export for this application
+				plan_to_do[time_block] = out_back_run("zenity --entry --text='Make a plan and start work' --ok-label='Start' --cancel-label='Exit'");
+			end
 		end
+		begin_time[time_block] = get_time();
+		plan_to_do[time_block] = string.gsub(plan_to_do[time_block], "\n", "");
 	end);
-	
-	-- Record thing
-	run("echo " .. get_time() .. " >> " .. thing_file);
-	zenity("entry", "Ok", "Cancel", "What do you plan to do?", ">> " .. thing_file);
 
 	-- Working
 	totale_work_time = totale_work_time + timing(function()
 		os.execute("sleep " .. (work_time * 60));
 	end);
 
-	-- Record thing
-	zenity("entry", "Ok", "Cancel", "Time is up, what have you done?", ">> " .. thing_file);
-	run("echo " .. get_time() .. " >> " .. thing_file);
-
+	-- Record have done
 	put_off_rest_time = put_off_rest_time + timing(function()
-		choose = ready("rest");
-		while choose == 0 do
-			exit();
-			choose = ready("rest");
-		end
+		have_done[time_block] = out_back_run("zenity --entry --text='Time is up, record what you do and have a rest.' --ok-label='Start' --cancel-label='Exit' --entry-text='" .. plan_to_do[time_block] .. "'");
+		have_done[time_block] = string.gsub(have_done[time_block], "\n", "");
+		end_time[time_block] = get_time();
 	end)
 
-	totale_rest_time = totale_rest_time + timing(function()
-		for i = 1, rest_time, 1 do
-			local status = run("zenity --info --title='" .. title .. "' --ok-label='Enter to speed up' --text='Back to work after " .. rest_time - i + 1 .. " minutes.' --no-wrap --timeout=60");
-			print(status);
-			if status ~= 5 then speed_up_rest_time = speed_up_rest_time + 1; end
-		end
+	local this_rest_time = timing(function()
+		choose = code_back_run("./tomato.lua --progress " .. rest_time * 60 .. " | zenity --progress --title='" .. title .. "' --cancel-label='Skip' --ok-label='Done' --auto-close");
+		if choose == 1 then 
+			-- Operation after user skipping the rest time.
+		end;
 	end);
+	totale_rest_time = totale_rest_time + this_rest_time;
+	speed_up_rest_time = speed_up_rest_time + rest_time - this_rest_time;
+
 end
 
-if arg[1] == "--count-time" then
-	count_time(arg[2] * 60)
+if arg[1] == "--progress" then
+	count_time(arg[2])
 	os.exit(0);
 end
 
+init();
 while true do
-	main();
+	loop();
 end
 
